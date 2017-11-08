be722bcd014d1562078792bf0408e3f11df95426
nastusya <nastasuper5@gmail.com>
2017-11-08 19:37:09 +0200

    laba 7

---
+++ comments.js
@@ -0,0 +1,21 @@
+
+function addReview() {
+    if ($('#name').val() === "" || $('#text').val() === "") {
+        alert('Заповніть всі поля');
+        return false;
+    } else {
+        var date = new Date;
+        var author = document.getElementById('name').value;
+        var text = document.getElementById('text').value;
+        var parentElem = document.getElementById('reviews-list');
+        var out = document.createElement('div');
+        out.id = 'comments';
+        out.innerHTML =
+            "<div class='container card'><br>" +
+            "   <span class='review-author'>" + author + "</span>" +
+            "   <span class='review-date'>" + date + "</span>" +
+            "   <p><br>" + text + "</p><br></div>";
+        parentElem.appendChild(out);
+        document.getElementById('form').reset();
+    }
+}
